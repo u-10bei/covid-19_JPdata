6a7848284297afc6659f385101d380d0eedaf867
u-10bei <holy19690206@gmail.com>
2021-08-10 16:54:01 +0900

    a

--- ReadCov19.R
+++ ReadCov19.R
@@ -2,4 +2,5 @@ library(dplyr)
 c("https://www.mhlw.go.jp/content/") -> mhlwurl
 c("https://ckan.open-governmentdata.org/dataset/") -> ckanurl
 ckanurl %>% paste("401005_kitakyushu_covid19_test_count/",sep="") -> cktqurl
+
 ckanurl %>% paste("401000_pref_fukuoka_covid19_exam/",sep="") -> pfukurl
