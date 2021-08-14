3b43f6b0eabd5ec3f8fd7e6c834736742d616d10
u-10bei <holy19690206@gmail.com>
2021-08-14 16:51:30 +0900

    Correcting markdowns

---
+++ CreateData410.R
@@ -0,0 +1,59 @@
+# ライブラリの読み込み
+ library(readr)
+ library(dplyr)
+
+# 全国データ
+# オープンデータのあるＵＲＬを変数に格納
+c("https://covid19.mhlw.go.jp/public/opendata/") -> mhlwURL
+
+# ネット上のファイル読み込み
+mhlwURL |> paste("newly_confirmed_cases_daily.csv",sep="") |>
+  read_csv() -> mhlwC
+mhlwURL |> paste("requiring_inpatient_care_etc_daily.csv",sep="") |>
+  read_csv() -> mhlwR
+mhlwURL |> paste("deaths_cumulative_daily.csv",sep="") |>
+  read_csv() -> mhlwD
+c("https://www.mhlw.go.jp/content/pcr_tested_daily.csv") |>
+  read_csv() -> mhlwT
+
+# 列名の再定義
+c("Date","Pref","Positive") -> colnames(mhlwC)
+c("Date","Pref","Inpatient","Discharged","Unconfirmed") -> colnames(mhlwR)
+c("Date","Pref","Deaths.cumulative") -> colnames(mhlwD)
+c("Date","Tesed") -> colnames(mhlwT)
+
+# 全国の状況のみ抽出
+mhlwC |> subset(Pref==c("ALL"),c(Date,Positive)) -> JPC
+mhlwR |> subset(Pref==c("ALL"),c(Date,Inpatient,Discharged,Unconfirmed)) -> JPR
+mhlwD |> subset(Pref==c("ALL"),c(Date,Deaths.cumulative)) -> JPD
+
+# ＣＳＶの結合、書き出し
+JPC |> left_join(mhlwT,by="Date") |>
+  left_join(JPR,by="Date") |>
+  left_join(JPD,by="Date") |>
+  write_csv("./data/COVID-19_JP.csv","")
+
+# 北九州データ
+# オープンデータのあるＵＲＬを変数に格納
+c("https://ckan.open-governmentdata.org/dataset/") -> ktqURL
+
+# ネット上のファイル読み込み
+ktqURL |> paste("bdb93937-3522-4c54-bf99-1ff24f51eb9d/",sep="") |>
+  paste("resource/f9bbb2bd-cb82-4395-8d7a-18566784c2bc/",sep="") |>
+  paste("download/401005_kitakyushu_covid19_test_count.csv",sep="") |>
+  read_csv(col_names=TRUE,locale=locale(encoding="SHIFT-JIS")) -> ktqT
+ktqURL |> paste("550f889f-f758-41e5-83c0-2e631d41af06/",sep="") |>
+  paste("resource/888afc2d-61ba-4f62-8494-c8022fcd8548/",sep="") |>
+  paste("download/401005_kitakyushu_covid19_confirm_negative.csv",sep="") |>
+  read_csv(col_names=TRUE,locale=locale(encoding="SHIFT-JIS")) -> ktqN
+
+# 列名の再定義
+c("Date","code","Pref","City","Tested") -> colnames(ktqT)
+c("Date","code","Pref","City","Negative") -> colnames(ktqN)
+
+# ＣＳＶの結合と陽性者数の作成、書き出し
+ktqT |> inner_join(ktqN,by="Date") |>
+  select(Date,Tested,Negative) |>
+  mutate(Positive=Tested-Negative) |>
+  mutate(Prate=Positive/Tested*100) |>
+  write_csv("./data/COVID-19_KTQTEST.csv","")
