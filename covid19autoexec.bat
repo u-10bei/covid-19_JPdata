ab386629dae4ba84d4819eddaabfdd0260312705
u-10bei <holy19690206@gmail.com>
2021-08-15 06:59:26 +0900

    Improved 2021/08/15

--- covid19autoexec.bat
+++ covid19autoexec.bat
@@ -1,6 +1,6 @@
 @rem "C:\Program Files\R\R-4.0.5\bin\rscript.exe" CreateData.R
 "C:\Program Files\R\R-4.1.1\bin\rscript.exe" CreateData410.R
-cd C:\Users\holy1\Documents\R\covid-19data\data
+cd C:\Users\holy1\Documents\R\covid-19_JPdata
 set A=Improved 
 set B=%date%
 set C="%A%%B%"
