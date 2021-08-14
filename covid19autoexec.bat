b4bd0ae847ec91a6dd920940d886e78ebe1782d9
u-10bei <holy19690206@gmail.com>
2021-08-15 06:57:51 +0900

    Improved 2021/08/15

--- covid19autoexec.bat
+++ covid19autoexec.bat
@@ -1,2 +1,9 @@
 @rem "C:\Program Files\R\R-4.0.5\bin\rscript.exe" CreateData.R
 "C:\Program Files\R\R-4.1.1\bin\rscript.exe" CreateData410.R
+cd C:\Users\holy1\Documents\R\covid-19data\data
+set A=Improved 
+set B=%date%
+set C="%A%%B%"
+git add .
+git commit -a -m %C%
+git push --all origin
