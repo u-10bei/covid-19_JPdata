8ad4e84712e8a1eceaabbc03bd4e9bba6a89ca2c
u-10bei <holy19690206@gmail.com>
2021-08-15 06:53:53 +0900

    change exec

--- covid19autoexec.bat
+++ covid19autoexec.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\R\R-4.0.5\bin\rscript.exe" CreateData.R
+@rem "C:\Program Files\R\R-4.0.5\bin\rscript.exe" CreateData.R
+"C:\Program Files\R\R-4.1.1\bin\rscript.exe" CreateData410.R
