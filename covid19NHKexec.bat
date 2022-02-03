bb978a6fc8fda32bcbe8480a70fab9c26ee817f7
u-10bei <holy19690206@gmail.com>
2022-02-04 07:15:39 +0900

    Improved 2022/02/04

--- covid19NHKexec.bat
+++ covid19NHKexec.bat
@@ -1,4 +1,4 @@
-"C:\Program Files\R\R-4.1.1\bin\rscript.exe" CreateNHK.R
+"C:\Program Files\R\R-4.1.2\bin\rscript.exe" CreateNHK.R
 cd C:\Users\holy1\Documents\R\covid-19_JPdata
 set A=Improved 
 set B=%date%
