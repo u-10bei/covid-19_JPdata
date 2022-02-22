f0dd4250d01912f2931c42119958fcf31b1a705d
u-10bei <holy19690206@gmail.com>
2022-02-22 23:55:45 +0900

    add batchfile

---
+++ covid19NHKexec.bat
@@ -0,0 +1,8 @@
+"C:\Program Files\R\R-4.1.2\bin\rscript.exe" CreateNHK.R
+cd C:\Users\holy1\Documents\R\covid-19_JPdata
+set A=Improved 
+set B=%date%
+set C="%A%%B%"
+git add .
+git commit -a -m %C%
+git push --all origin
